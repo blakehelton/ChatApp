94ed6e868b8a9b15e650e18a16b0a7a6df127fbd
Armeen Mahdian <mahdianarmeen@gmail.com>
2021-02-26 11:29:17 -0600

    Get token from env var

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
+#include <fmt/core.h>
+#include <cstdlib>
 #include <sleepy_discord.h>
 
 class MyClientClass : public SleepyDiscord::DiscordClient {
 public:
   using SleepyDiscord::DiscordClient::DiscordClient;
   void onMessage(SleepyDiscord::Message message) override {
-    if (message.startsWith("whcg hello"))
+    if (message.startsWith("$hello"))
       sendMessage(message.channelID, "Hello " + message.author.username);
   }
 };
 
 int main() {
-  MyClientClass client("token", SleepyDiscord::USER_CONTROLED_THREADS);
-  client.run();
+  try {
+    auto const token = std::getenv("DISCORD_BOT_TOKEN");
+    MyClientClass client(token, SleepyDiscord::USER_CONTROLED_THREADS);
+    client.run();
+  } catch (std::exception const& e) {
+    fmt::print("Caught exception: {}\n", e.what());
+  }
 }
